7e1e2de8adad130f8ca84b9cea4a347af1569b4d
Ventsislav Kiskinov <venci34@gmail.com>
2022-01-15 14:34:05 +0200

    asdasdsa

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import TextArea from "./textarea";
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <div className="box">
         <div className="field">
           <div className="control">
+            <TextArea />
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./index.css";
 
 import App from "./App";
-import TextArea from "./textarea";
 import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
@@ -9,7 +8,6 @@ import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <TextArea />
   </React.StrictMode>,
   document.getElementById("root")
 );
